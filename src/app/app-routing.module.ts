6fb9a9c16bf76d0e2aafe77179bac19f1765b3f5
Unknown <chr.bachler@googlemail.com>
2018-04-26 18:26:53 +0200

    Entwicklung Frontend: Grundlegende Funktionen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AccountComponent } from './account/account.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login' pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'overview', component: OverviewComponent },
   { path: 'account', component: AccountComponent }
