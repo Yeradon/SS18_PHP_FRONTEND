4a64e22fe7d15d2b6f6e2bd2cdd176adda46555c
Unknown <chr.bachler@googlemail.com>
2018-04-26 12:41:01 +0200

    Entwicklung Frontend: Grundlegende Funktionen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
-  { path: '', component: OverviewComponent }
+  { path: '', component: OverviewComponent },
+  { path: 'account', component: AccountComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { OverviewComponent } from './overview/overview.component';
+import { MenuComponent } from './menu/menu.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    MenuComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
