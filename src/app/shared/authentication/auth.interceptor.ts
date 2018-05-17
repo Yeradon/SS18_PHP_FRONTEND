aefa9b0fcc379245aae5f54380152307d019e128
Unknown <yeradon@gmail.com>
2018-05-17 12:07:57 +0200

    prettier

--- src/app/shared/authentication/auth.interceptor.ts
+++ src/app/shared/authentication/auth.interceptor.ts
@@ -4,30 +4,42 @@ import {
 } from '@angular/common/http';
 
 import {AuthenticationService} from "./authentication.service";
-import {catchError} from "rxjs/operators";
-import {throwError} from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 import {ErrorObservable} from "rxjs-compat/observable/ErrorObservable";
 import { Observable } from 'rxjs/Rx';
 import { MessageService } from '../message/message.service';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthenticationService, private message: MessageService) {}
+  constructor(
+    private auth: AuthenticationService,
+    private message: MessageService
+  ) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if(this.auth.isLoggedIn) {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (this.auth.isLoggedIn) {
       const newReq = req.clone({
         headers: req.headers.set('Authentication', this.auth.getToken())
       });
-      return next.handle(newReq).pipe(catchError((err: HttpEvent<any>, caught) => {
-        if(err instanceof HttpErrorResponse) {
-          if(err.status == 401) {
-            this.auth.logout();
-            this.message.add("Deine Session ist abgelaufen");
+      return next
+        .handle(newReq)
+        .pipe(
+          
+        catchError((err: HttpEvent<any>, caught) => {
+              if  (err instanceof HttpErrorResponse) {
+                if  (err.status == 401) {
+                  this.auth.logout();
+                  this.message.add('Deine Session ist abgelaufen');
+                }
+                return throwError(err);
           }
-          return throwError(err);
-        }
-      }));
+            })
+          )
+        ;
     }
     return next.handle(req);
   }
