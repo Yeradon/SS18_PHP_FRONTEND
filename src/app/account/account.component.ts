87912e6ca5e3e359ab7fa6104c308ec3ae680014
Unknown <chr.bachler@googlemail.com>
2018-05-29 12:08:21 +0200

    wip: Account bearbeiten

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -19,6 +19,10 @@ export class AccountComponent implements OnInit {
   public showDeleteAccount: boolean = false;
   public textChangePassword: string = TEXT_CHANGE_PASSWD;
   public textDeleteAccount: string = TEXT_DELETE_ACCOUNT;
+  public oldPasswd: string;
+  public newPasswd: string;
+  public newPasswdRepeat: string;
+  public isLoading: boolean = false;
 
   constructor(private userService: UserService, private authService: AuthenticationService, private messageService: MessageService) {}
 
@@ -44,24 +48,62 @@ export class AccountComponent implements OnInit {
 
   updateAccount() {
 
-    console.log('account.component: updating: '+this.user);
-    this.userService.modifyUser(this.user, {
-      name: this.user.name,
-      surname:this.user.surname
-    }).then( user => {
-        console.log('account.component: success!');
+    console.log('updateAccount: START');
+    let data = {};
+
+    if (this.showChangePassword) {
+      console.log('updateAccount: password change requested');
+      if (this.newPasswd != this.newPasswdRepeat) {
+        this.messageService.add('Das neue Passwort und die Wiederholung des neuen Passworts stimmen nicht überein.');
+        console.log('updateAccount: invalid user input');
+        console.log('updateAccount: END');
+        return;
+      } else {
+        console.log('updateAccount: password change initialized');
+        data = {
+          name: this.user.name,
+          surname: this.user.surname,
+          oldPassword: this.oldPasswd,
+          newPassword: this.newPasswd
+        }
+      }
+    } else {
+      console.log('updateAccount: user info change only');
+      data = {
+        name: this.user.name,
+        surname: this.user.surname
+      }
+    }
+
+    console.log('updateAccount: sending request to server');
+    this.isLoading = true;
+    this.userService.modifyUser(this.user, data).then( user => {
+        console.log('updateAccount: request successfull');
+        this.isLoading = false;
+        this.messageService.add('Änderungen wurden gespeichert.');
     }, (err) => {
-      this.messageService.add("Fehler beim Löschen des Benutzeraccounts: "+ err);
+        console.log('updateAccount: request failed: '+err);
+        this.isLoading = false;
+        this.messageService.add("Fehler: "+ err);
+        this.userService.loadUser().then((user) => {
+          this.user = user;
+        });
     });
+    console.log('updateAccount: END');
   }
 
   removeAccount() {
+    console.log('removeAccount: START');
+    console.log('removeAccount: sending request to server');
     this.userService.loadUser().then(user => {
       this.userService.deleteUser(user).then( user => {
+          console.log('removeAccount: request successfull! user will be logged out');
           this.authService.logout();
       }, (err) => {
+        console.log('removeAccount: request failed: '+err);
         this.messageService.add("Fehler beim Löschen des Benutzeraccounts: "+ err);
       })
     });
+    console.log('removeAccount: END');
   }
 }
